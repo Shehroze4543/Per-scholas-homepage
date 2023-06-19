2b48cbeec9580950fa1f4c06aec9a5e5ee59f78f
Shehroze4543 <Shehroze4543@gmail.com>
2023-06-18 20:17:38 -0400

    finished mobile screen part

---
+++ app.js
@@ -0,0 +1,52 @@
+let dot_1 = document.querySelector(".alumni-work-img-1");
+let dot_2 = document.querySelector(".alumni-work-img-2");
+let dot_3 = document.querySelector(".alumni-work-img-3");
+let dot_4 = document.querySelector(".alumni-work-img-4");
+let dot_5 = document.querySelector(".alumni-work-img-5");
+
+function first_slide() {
+  dot_1.classList.add("hidden");
+  dot_2.classList.add("hidden");
+  dot_3.classList.add("hidden");
+  dot_4.classList.add("hidden");
+  dot_5.classList.add("hidden");
+
+  dot_1.classList.remove("hidden");
+}
+
+function second_slide() {
+  dot_1.classList.add("hidden");
+  dot_2.classList.add("hidden");
+  dot_3.classList.add("hidden");
+  dot_4.classList.add("hidden");
+  dot_5.classList.add("hidden");
+
+  dot_2.classList.remove("hidden");
+}
+function third_slide() {
+  dot_1.classList.add("hidden");
+  dot_2.classList.add("hidden");
+  dot_3.classList.add("hidden");
+  dot_4.classList.add("hidden");
+  dot_5.classList.add("hidden");
+
+  dot_3.classList.remove("hidden");
+}
+function fourth_slide() {
+  dot_1.classList.add("hidden");
+  dot_2.classList.add("hidden");
+  dot_3.classList.add("hidden");
+  dot_4.classList.add("hidden");
+  dot_5.classList.add("hidden");
+
+  dot_4.classList.remove("hidden");
+}
+function fifth_slide() {
+  dot_1.classList.add("hidden");
+  dot_2.classList.add("hidden");
+  dot_3.classList.add("hidden");
+  dot_4.classList.add("hidden");
+  dot_5.classList.add("hidden");
+
+  dot_5.classList.remove("hidden");
+}
